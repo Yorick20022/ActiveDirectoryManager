36d7c8e9bf6866e660ff69f3fe5b3846010cff4a
Yorick <125297057+Yorick20022@users.noreply.github.com>
2024-01-09 08:59:54 +0100

    fix: put state to normal before doing shit because it wasnt showing

--- functions/listDisabledUsers.py
+++ functions/listDisabledUsers.py
@@ -9,6 +9,7 @@ def listDisabledUsers(self):
         scriptOutput = subprocess.check_output(['powershell', '-File', powershell_script], text=True, stderr=subprocess.STDOUT)
 
         # Update the text in the custom Textbox with the PowerShell script output
+        self.textbox.configure(state= "normal")
         self.textbox.delete("0.0", "end")
         self.textbox.insert("0.0", scriptOutput)
     except subprocess.CalledProcessError as e:
